b1bfb43a8bf827a556a6443bc385ce70eeca7984
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-07-15 15:32:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,20 @@ function setup(){
     can.position(450);
     cam=createCapture(VIDEO);
     cam.hide();
+    classify=ml5.imageClassifier('mobileNet',modlod);
 }
 function draw(){
     image(cam,0,0,350,300);
-
+    classify.Classify(cam,grrr);
+}
+function modlod(){
+    console.log("model is loaded");
+}
+function grrr(error,res){
+    if(error){
+        console.error(error);
+    }
+    else{
+     console.log(res);   
+    }
 }
